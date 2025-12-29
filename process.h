6e682b8dbb8d0831fdf731654e9ca42ecb09e8f4
Enrtarr <120576263+Enrtarr@users.noreply.github.com>
2025-12-29 19:11:21 +0100

    Update process.h

--- process.h
+++ process.h
@@ -1,31 +1,31 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
-#include <stddef.h>
+#include <stdio.h>
 
+/* Process information structure */
 typedef struct {
     int pid;
     char user[32];
     double cpu_usage;
     double mem_usage;
-    char command[256];
     char state;
+    char command[256];
 } process_info_t;
 
+/* Linked list element for process information */
 typedef struct process_elem {
     process_info_t process;
     struct process_elem *next;
 } process_elem;
 
+/* Linked list for process information */
 typedef struct {
     process_elem *head;
 } process_list;
 
-/* Crée une liste chainée contenant les processus locaux */
 process_list *create_process_list(void);
-
-/* Libère toute la mémoire associée à la liste chainée */
+process_list *create_process_list_from_stream(FILE *fp);
 void free_process_list(process_list *list);
 
 #endif
-
